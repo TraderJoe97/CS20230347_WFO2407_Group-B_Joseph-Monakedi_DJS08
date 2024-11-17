67951892c280cc3ffca98ce14720df8da064b5be
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2024-11-17 23:55:40 +0200

    fetch van details

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function VanDetail() {
     const params = useParams();
-    console.log(params);
+    useEffect(() => {
+        fetch(`/api/vans/${params.VanId}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [params.VanId])
+
     return (
         <div>
             <h1>Van detail page goes here</h1>
