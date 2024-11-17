db6d0352263a7aff001bea769565df4ae36b222c
Joseph Monakedi <99967046+TraderJoe97@users.noreply.github.com>
2024-11-17 23:47:50 +0200

    get and console log params

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route, NavLink} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
 
 import "./server"
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:vanId" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
