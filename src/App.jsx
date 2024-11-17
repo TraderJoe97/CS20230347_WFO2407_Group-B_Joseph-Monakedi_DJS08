90f54abfd3c07b6c770d7061de912875f96a0eab
Joseph Monakedi <jimonakedi@gmail.com>
2024-11-18 01:03:19 +0200

    create host pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans/Vans";
 import VanDetail from "./pages/Vans/VanDetail";
+import HostLayout from "./components/HostLayout";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
@@ -20,9 +21,11 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/HostLayout.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function HostLayout () {
+    return (
+        <nav className="host-nav">
+            <Link to="/host">Dashboard</Link>
+            <Link to="/host/income">Income</Link>
+            <Link to="/host/Reviews">Reviews</Link>
+        </nav>
+    )
+}
