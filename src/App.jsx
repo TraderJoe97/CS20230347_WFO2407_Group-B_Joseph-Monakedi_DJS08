39a89c8290ea7689e12622bd48973b969eb4b8da
Joseph Monakedi <jimonakedi@gmail.com>
2024-11-18 13:45:41 +0200

    404 page

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import HostVanDetail from "./pages/Host/HostVanDetail";
 import HostVanInfo from "./pages/Host/HostVanInfo";
 import HostVanPricing from "./pages/Host/HostVanPricing";
 import HostVanPhotos from "./pages/host/HostVanPhotos";
+import NotFound from "./pages/NotFound";
 
 import "./server"
 function App() {
@@ -37,6 +38,7 @@ function App() {
               <Route path="photos" element={<HostVanPhotos />} /> 
             </Route>
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
